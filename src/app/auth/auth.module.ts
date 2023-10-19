472d8f595ff0a7135c9aa51fed2fc878f061b2f0
Diego Medina <diegocab456@gmail.com>
2023-10-19 00:19:09 -0300

    login page

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
@@ -14,8 +15,9 @@ import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component
     AuthLayoutComponent
   ],
   imports: [
+    ReactiveFormsModule,
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
   ]
 })
 export class AuthModule { }
